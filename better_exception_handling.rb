b9991e7d20037ab7eab0b5630f857449343cfef6
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2014-02-14 16:20:56 +0200

    renaming

--- better_exception_handling.rb
+++ better_exception_handling.rb
@@ -1,6 +1,6 @@
 require "action_mailer"
-require 'better_exception_notifier'
-require 'better_exception_handling'
+require 'lib/better_exception_notifier'
+require 'lib/better_exception_handling'
 
 require 'request_exception_handler'
 ActionController::Base.send :include, RequestExceptionHandler
