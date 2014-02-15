7d7d0037ddf1f34391db06b59afc915dc0353960
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2014-02-15 18:39:15 +0200

    versoning fix

--- lib/version.rb
+++ lib/version.rb
@@ -1,8 +1,8 @@
 module BetterExceptionHandling
 	class Version
-		MAJOR = 1
-  	MINOR = 4
-  	PATCH = 0
+		MAJOR = 0
+  	MINOR = 0
+  	PATCH = 1
 
   	def self.to_s
     	"#{MAJOR}.#{MINOR}.#{PATCH}"
